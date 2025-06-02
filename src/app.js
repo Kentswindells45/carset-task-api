0e2521f98504c736add5393e88fdfe81d1faa3f0
Kentswindells45 <kentswindells@gmail.com>
2025-06-02 14:13:34 +0000

    removed  middleware auth  declaration

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const authRoutes = require('./routes/authRoutes');
 const taskRoutes = require('./routes/taskRoutes');
-const { verifyToken } = require('./middleware/authMiddleware');
 const errorHandler = require('./middleware/errorHandler');
 
 dotenv.config();
